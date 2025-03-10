2af33a9c7723915cf4ff14c44dd74f5acf992448
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-03-11 00:04:22 +0600

    added time line theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
 import Service from "./components/Service"
- import Skills from "./components/Skills"
+import Skills from "./components/Skills"
 
 const App = () => {
   return (
     <div className=" overflow-hidden w-full h-full bg-gradient-to-r from-black to-[#1B0132] text-white">
-      <Navbar/>
-      <Hero/>
-      <Service/>
-      <Skills/>
-      <Contact/>
-      <Footer/>
+      <Navbar />
+      <Hero />
+      <Service />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import { IoArrowBackCircle } from "react-icons/io5";
+import { FaCircleArrowRight } from "react-icons/fa6";
+import { ImGithub } from "react-icons/im";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import card1 from "../assets/gaming.jpg";
+import card2 from "../assets/show.jpg";
+
+const Projects = () => {
+    return (
+        <div id="projects" className="py-20">
+            <div>
+                <h1 className="text-center text-5xl text-white">Vertical Timeline</h1>
+            </div>
+            <div>
+                <VerticalTimeline className="bg-transparent overflow-hidden">
+                    {/* First Project */}
+                    <VerticalTimelineElement
+                        className="cursor-pointer overflow-hidden"
+                        contentStyle={{ background: "transparent" }}
+                        contentArrowStyle={{ borderRight: "7px solid transparent" }}
+                        icon={
+                            <IoArrowBackCircle className="text-3xl text-white bg-orange-600 w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
+                        }
+                    >
+                        <div className="back">
+                            <img src={card1} alt="Project Image" />
+                            <a href="https://rifat654.github.io/Gaming-World/">
+                                <ImGithub className="text-xl" />
+                            </a>
+                        </div>
+                    </VerticalTimelineElement>
+
+                    {/* Second Project */}
+                    <VerticalTimelineElement
+                        className="cursor-pointer overflow-hidden"
+                        contentStyle={{ background: "transparent" }}
+                        contentArrowStyle={{ borderRight: "7px solid transparent" }}
+                        icon={
+                            <FaCircleArrowRight className="text-3xl text-white bg-orange-600 w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
+                        }
+                    >
+                        <div className="back">
+                            <img src={card2} alt="Project Image" />
+                            <a href="https://rifat654.github.io/Tailwindcss-web-page/">
+                                <ImGithub className="text-xl" />
+                            </a>
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
