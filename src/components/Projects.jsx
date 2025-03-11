46d95197a418b1090f413b05e5a71d77c0ff1a4e
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-03-11 12:55:56 +0600

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
-import { IoArrowBackCircle } from "react-icons/io5";
-import { FaCircleArrowRight } from "react-icons/fa6";
+import { RiFocus3Fill } from "react-icons/ri";
 import { ImGithub } from "react-icons/im";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import card1 from "../assets/gaming.jpg";
-import card2 from "../assets/show.jpg";
+import card1 from "../assets/American-express.jpg"
+import card2 from "../assets/gaming.jpg";
+import card3 from "../assets/show.jpg";
 
 const Projects = () => {
     return (
@@ -20,12 +20,12 @@ const Projects = () => {
                         contentStyle={{ background: "transparent" }}
                         contentArrowStyle={{ borderRight: "7px solid transparent" }}
                         icon={
-                            <IoArrowBackCircle className="text-3xl text-white bg-orange-600 w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
+                            <RiFocus3Fill className="text-3xl text-purple-700 bg-white w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
                         }
                     >
                         <div className="back">
                             <img src={card1} alt="Project Image" />
-                            <a href="https://rifat654.github.io/Gaming-World/">
+                            <a href="https://react-animation-0241.netlify.app/">
                                 <ImGithub className="text-xl" />
                             </a>
                         </div>
@@ -37,19 +37,37 @@ const Projects = () => {
                         contentStyle={{ background: "transparent" }}
                         contentArrowStyle={{ borderRight: "7px solid transparent" }}
                         icon={
-                            <FaCircleArrowRight className="text-3xl text-white bg-orange-600 w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
+                            <RiFocus3Fill className="text-3xl text-purple-700 bg-white w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
                         }
                     >
                         <div className="back">
                             <img src={card2} alt="Project Image" />
+                            <a href="https://rifat654.github.io/Gaming-World/">
+                                <ImGithub className="text-xl" />
+                            </a>
+                        </div>
+                    </VerticalTimelineElement>
+
+                    {/* third Project */}
+                    <VerticalTimelineElement
+                        className="cursor-pointer overflow-hidden"
+                        contentStyle={{ background: "transparent" }}
+                        contentArrowStyle={{ borderRight: "7px solid transparent" }}
+                        icon={
+                            <RiFocus3Fill className="text-3xl text-purple-700 bg-white w-full h-full rounded-full cursor-pointer hover:text-green-700 hover:bg-white transition-all duration-300" />
+                        }
+                    >
+                        <div className="back">
+                            <img src={card3} alt="Project Image" />
                             <a href="https://rifat654.github.io/Tailwindcss-web-page/">
                                 <ImGithub className="text-xl" />
                             </a>
                         </div>
                     </VerticalTimelineElement>
+
                 </VerticalTimeline>
             </div>
-        </div>
+        </div >
     );
 };
 
