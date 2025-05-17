4d3613f2061a09410cea8b77222502cd0bca9eeb
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-17 14:01:40 +0600

    added service data

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+// Services data for the FAQ section
+export const data = [
+    {
+        id: 1,
+        question: "Custom Website Development",
+        answer: "Build modern, responsive, and high-performance websites from scratch."
+    },
+    {
+        id: 2,
+        question: "UI/UX Design Implementation",
+        answer: "Improve site speed, SEO, and accessibility for a better user experience."
+    },
+    {
+        id: 3,
+        question: "Performance Optimization",
+        answer: "Improve site speed, SEO, and accessibility for a better user experience."
+    },
+    {
+        id: 4,
+        question: "Responsive Web Design",
+        answer: "Ensure your website looks great on all screen sizes and devices."
+    },
+    {
+        id: 5,
+        question: "API Integration",
+        answer: "Seamlessly connect your frontend with backend services and third-party APIs."
+    },
+  
+        
+]
